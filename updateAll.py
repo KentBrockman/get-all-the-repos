9834c71a57cb52853d2de76d3df8f5bb066bafbc
Kent Brockman <KBrockman@matrix-solutions.com>
2016-05-21 18:56:45 -0600

    refactor - ready to clone new repos

--- updateAll.py
+++ updateAll.py
@@ -5,25 +5,35 @@ import json
 import urllib2
 import subprocess
 
+def listdir_nohidden(path):
+        for f in os.listdir(path):
+                    if not f.startswith('.'):
+                                    yield f
+
 print 'Get all repositories'
-data = urllib2.urlopen("https://api.github.com/orgs/openaginitiative/repos")
-repositories = json.load(data)
-repos = {}
+data = json.load(urllib2.urlopen("https://api.github.com/orgs/openaginitiative/repos"))
+remoteRepos = {}
 
-for repo in repositories:
-     repos[repo["name"]] = False
+for repo in data:
+    remoteRepos[repo["name"]] = {"remoteUrl": repo["ssh_url"], "pulled": False}
 
-items = os.listdir('.') 
+items = listdir_nohidden('.') 
 for item in items:
     if os.path.isdir(item):
+        print item
+
         command = "cd {0}".format(item)
         os.chdir(item)
         subprocess.call(["git up"], shell=True)
         os.chdir("..")
-        repos[item] = True
 
-if False in repos.values():
+        if remoteRepos.has_key(item):
+           remoteRepos[item]["pulled"] = True
+        else:
+            print "Repository doesn't exist on github: {0}".format(item)
+
+if any(not f["pulled"] for f in remoteRepos.values()):
     print "You are missing the following repositories:"
-    for repo in repos:
-        if repos[repo] is False:
-            print repo
+    for k in remoteRepos:
+        if remoteRepos[k]["pulled"] is False:
+            print k
